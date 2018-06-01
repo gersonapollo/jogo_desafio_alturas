57e6287a16062e9cd14eb6d2afc2bd8cc762d988
Gerson Reis <gersonapollo@gmail.com>
2018-06-01 01:09:12 -0300

    Criacao do jogo estilo flapBird

---
+++ Assets/scripts/aviao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aviao : MonoBehaviour {
+    Rigidbody2D fisica;
+
+	private void Awake()
+	{
+        fisica = this.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	private void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            this.impulsionar();
+        }
+	}
+
+    private void impulsionar()
+    {
+        fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+}
